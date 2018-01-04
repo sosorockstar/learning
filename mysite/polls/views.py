a3c53c5a69c15d11992d89aa1bff64c619a4cd67
nick zhou <babygiant@126.com>
2018-01-04 11:24:31 +0800

    writing your first django app, part6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
